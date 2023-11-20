1c5fee4a1ed93c21e926aecd8de080c1979f5cb9
John Moore <jmoore@aeci.org>
2023-11-19 20:37:49 -0600

    today's leetcode

---
+++ practice/Practice.Implementation/Leetcode_20231119.cs
@@ -0,0 +1,25 @@
+namespace Practice.Implementation;
+public class Leetcode_202311119
+{
+    // Given an integer array nums, your goal is to make all elements in nums equal. To complete one operation, follow these steps:
+
+    // Find the largest value in nums. Let its index be i (0-indexed) and its value be largest. If there are multiple elements with the largest value, pick the smallest i.
+    // Find the next largest value in nums strictly smaller than largest. Let its value be nextLargest.
+    // Reduce nums[i] to nextLargest.
+    // Return the number of operations to make all elements in nums equal.
+
+    public int ReductionOperations(int[] nums) 
+    {
+        var sorted = nums.OrderBy(n => n).ToList(); // O(n log n)
+        int totalOps = 0;
+        for(int i = nums.Length-1; sorted[0] != sorted[i]; i--) // O(n)
+        {
+            if(sorted[i] != sorted[i-1])
+            {
+                // reduce i and everything after it
+                totalOps = totalOps + (nums.Length-i);
+            }
+        }
+        return totalOps;
+    }
+}
---
+++ practice/Practice.Tests/Leetcode_20231119Tests.cs
@@ -0,0 +1,27 @@
+namespace Practice.Tests;
+public class Leetcode_20231119Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        int[] input = new[] { 5,1,3 };
+        var result = new Leetcode_202311119().ReductionOperations(input);
+        Assert.Equal(3, result);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        int[] input = new[] { 1,1,1 };
+        var result = new Leetcode_202311119().ReductionOperations(input);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        int[] input = new[] { 1,1,2,2,3 };
+        var result = new Leetcode_202311119().ReductionOperations(input);
+        Assert.Equal(4, result);
+    }
+}
