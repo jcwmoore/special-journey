eae3e04851480ce531076ffff757b0261085c0af
John Moore <jmoore@aeci.org>
2023-11-19 20:06:25 -0600

    adding reference to implementation

--- tfin/init.ps1
+++ tfin/init.ps1
@@ -3,5 +3,6 @@ dotnet new classlib --name Triumph.Implementation --output Triumph.Implementatio
 dotnet sln Triumph.sln add .\Triumph.Implementation\Triumph.Implementation.csproj
 dotnet new xunit --name Triumph.Tests --output Triumph.Tests
 dotnet sln Triumph.sln add .\Triumph.Tests\Triumph.Tests.csproj
+dotnet add .\Triumph.Tests\Triumph.Tests.csproj reference .\Triumph.Implementation\Triumph.Implementation.csproj
 dotnet restore
 dotnet build
